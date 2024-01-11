0c71d5b88cb77e8f2eb0a609eafbdcf2e8377b4d
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-11 11:51:15 -0300

    Update ExibindoNumerosPares.java

--- ExibindoNumerosPares/ExibindoNumerosPares.java
+++ ExibindoNumerosPares/ExibindoNumerosPares.java
@@ -1,12 +1,13 @@
 package ExibindoNumerosPares;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class ExibindoNumerosPares {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
