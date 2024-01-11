c3567b79ddfcad9736791f4f59d76e95d270863f
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-11 13:54:46 -0300

    Update EntradaESaidaCPF.java

--- EntradaESaidaCPF/EntradaESaidaCPF.java
+++ EntradaESaidaCPF/EntradaESaidaCPF.java
@@ -1,3 +1,40 @@
+/*
+====================================================
+Iniciando a programação em Java
+====================================================
+2/3 - Entrada e Saída CPF
+====================================================
+
+Desafio
+Elabore um programa que possuas as características abaixo:
+- Leia os dados de um CPF no formato XXX.YYY.ZZZ-DD;
+- Imprima os quatro grupos numéricos, sendo um valor por linha.
+
+Entrada
+A entrada consiste vários arquivos de teste e cada um possuindo uma linha com formato XXX.YYY.ZZZ-DD, onde XXX, YYY, ZZZ, DD são números inteiros.
+
+Saída
+Para cada arquivo da entrada, tem que ter um arquivo de saída com quatro linhas, e em cada linha um número inteiro de acordo com procedimento 2 descrito no Desafio. Confira o exemplo abaixo:
+
+--------------------------------------------
+| Exemplos de Entrada  | Exemplos de Saída |
+| 000.000.000-00       | 000               |
+|                      | 000               |
+|                      | 000               |
+|                      | 00                |
+--------------------------------------------
+
+--------------------------------------------
+| Exemplos de Entrada  | Exemplos de Saída |
+| 320.025.102-01       | 320               |
+|                      | 025               |
+|                      | 102               |
+|                      | 01                |
+--------------------------------------------
+
+
+SOLUÇÃO ABAIXO: */
+
 import java.util.Scanner;
 import java.util.Locale;
 
