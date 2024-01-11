ae94c01a38962adde9b641f7de5f8b5098c81c81
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-11 11:41:23 -0300

    Update EntradaESaidaCPF.java

--- EntradaESaidaCPF/EntradaESaidaCPF.java
+++ EntradaESaidaCPF/EntradaESaidaCPF.java
@@ -6,16 +6,16 @@ public class EntradaESaidaCPF{
 		
 		Locale.setDefault(new Locale("en", "US"));
 
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
 		String[] cpf = new String[4]; 
 
-		cpf = sc.nextLine().split("\\.|-");
+		cpf = scan.nextLine().split("\\.|-");
 
     for (int i = 0; i < 4; i++) {
       System.out.printf("%s\n",cpf[i]);
     }
 
-		sc.close();
+		scan.close();
 	}
 }
