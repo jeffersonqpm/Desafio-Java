8106321417967db92971ed365361eba3502915e6
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-11 11:35:49 -0300

    Create EntradaESaidaCPF.java

---
+++ EntradaESaidaCPF/EntradaESaidaCPF.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaESaidaCPF{
+
+
+    public static void main(String[] args) {
+        
+
+        Locale.setDefault(new Locale("en", "US"));
+        Scanner scan = new Scanner(System.in);
+        String[] cpf = new String[4];
+
+        cpf =scan.nextLine().split("\\.|-");
+
+        for(int i =0; i<4; i++){
+            System.out.printf("%s\n", cpf[i]);
+
+        }
+        scan.close();
+
+
+    }
+}
