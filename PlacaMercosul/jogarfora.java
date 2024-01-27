b1bbd8b8e6b8a60a6a765d87f83fd760f434a3cc
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-27 15:13:39 -0300

    Create jogarfora.java

---
+++ PlacaMercosul/jogarfora.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class jogarfora {
+
+
+    public static void main(String[] args) {
+        
+        boolean continuar = true;
+        Scanner scan = new Scanner(System.in);
+        int quantidade = 4;
+        String[] listaNome = new String[quantidade];
+        System.out.println("Digite um nome ou (0) ZERO para sair");
+        listaNome[]
+        
+
+
+        while (continuar) {
+
+            for(){
+            
+            }
+            
+        }
+    }
+    
+}
