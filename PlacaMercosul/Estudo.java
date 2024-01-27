5162f8f35492059b8a0fc26f13a6e18e5fffaf89
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-27 13:59:55 -0300

    Update Estudo.java

--- PlacaMercosul/Estudo.java
+++ PlacaMercosul/Estudo.java
@@ -1,52 +1,53 @@
 import java.util.Scanner;
 
 public class Estudo {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        boolean continuar = true;
-        System.out.println("Digite a placa ou ZERO para sair: ");
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a placa");
         String placa = scan.nextLine();
 
         System.out.println(concatenar(placa));
 
     }
 
-    private static String concatenar(String placa){
+    private static String concatenar(String placa) {
 
         String parte1 = esquerda(placa, 5);
-        String parte2 = esquerda(diretia(placa, 2), 1);
-        String parte3 = diretia(placa, 1);
+        String parte2 = esquerda(direita(placa, 2), 1);
+        String parte3 = direita(placa, 1);
 
         switch (parte2) {
-            case "0": parte2 = "A"; break;
-            case "1": parte2 = "B"; break;
-            case "2": parte2 = "C"; break;
-            case "3": parte2 = "D"; break;
-            case "4": parte2 = "E"; break;
-            case "5": parte2 = "F"; break;
-            case "6": parte2 = "G"; break;
-            case "7": parte2 = "H"; break;
-            case "8": parte2 = "I"; break;
-            case "9": parte2 = "J"; break;
-               
-        
-            default:
-            System.out.println("Inválido!");
-                break;
+            
+                case "0": parte2 = "A"; break;
+                case "1": parte2 = "B"; break;
+                case "2": parte2 = "C"; break;
+                case "3": parte2 = "D"; break;
+                case "4": parte2 = "E"; break;
+                case "5": parte2 = "F"; break;
+                case "6": parte2 = "G"; break;
+                case "7": parte2 = "H"; break;
+                case "8": parte2 = "I"; break;
+                case "9": parte2 = "J"; break;
+                default:
+                System.out.println("Inválido");
+
         }
 
         return (parte1 + parte2 + parte3);
-    }
-
-    private static String esquerda(String texto, int numeroCaractere){
-
-        return texto.substring(0,numeroCaractere);
 
     }
 
-    private static String diretia(String texto, int numeroCaractere){
-        return texto.substring(texto.length() - numeroCaractere);
+    private static String esquerda(String texto, int numeroDecaractere) {
+
+        return texto.substring(0, numeroDecaractere);
+
+    }
+
+    private static String direita(String texto, int numeroDecaractere) {
+        return texto.substring(texto.length() - numeroDecaractere);
     }
 
 }
